cd9f60d7cc93755f086e0a281a92594c4cc4af9b
Marcos <100496072@alumnos.uc3m.es>
2024-02-07 11:13:15 +0100

    MArcos Hehcio

--- UC3MTravel/HotelManager.py
+++ UC3MTravel/HotelManager.py
@@ -17,6 +17,7 @@ class HotelManager:
                     Suma += 1 + (Resultado - 10)
                 else:
                     Suma += Resultado
+
             else:
                 Suma += int(x[contador])
             contador += 1
